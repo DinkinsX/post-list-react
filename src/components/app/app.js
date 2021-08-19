069ee3f80ca856b62e3d44a543610cb9ac439772
Daniel <dan767@yandex.ru>
2021-08-19 16:07:56 +0700

    deleteItem

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,21 +5,64 @@ import PostStatusFilter from '../post-status-filter';
 import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 import './app.css'
+import styled from 'styled-components';
 
 
-const App = () => {
-    return (
-    <div className="app">
-        <AppHeader/>
-        <div className="search-panel d-flex">
-            <SearchPanel/>
-            <PostStatusFilter/>
-        </div>
-        <PostList/>
-        <PostAddForm/>
-    </div>
+const AppBlock = styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+`;
 
-    );
+// const StyledAppBlock = styled(AppBlock)`
+//     background-color: grey;
+// `;
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : [
+                {
+                    label: 'Mama mamaochka',
+                    important: false,
+                    id: 'dss'
+                },
+                {
+                    label: 'Mama mamaochka',
+                    important: true,
+                    id: 'wdw'
+                },
+                {
+                    label: 'Mama mamaochka',
+                    important: false,
+                    id: 'ERVV'
+                }
+            ]
+        };
+        this.deleteItem = (id) => {
+            this.setState(({data}) => {
+                const index = data.findIndex(elem => elem.id === id);
+                const before = data.slice(0, index);
+                const after = data.slice(index + 1);
+                const newArr = [...before, ...after];
+                return {
+                    data: newArr
+                }
+            });
+        }
+    }
+
+    render() {    
+        return (
+        <AppBlock>
+            <AppHeader/>
+            <div className="search-panel d-flex">
+                <SearchPanel/>
+                <PostStatusFilter/>
+            </div>
+            <PostList posts={this.state.data} onDelete={this.deleteItem}/>
+            <PostAddForm/>
+        </AppBlock>
+        )
+    }
 }
-
-export default App;
--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from 'reactstrap';
 
 const PostStatusFilter = () => {
     return (
 
         <div className="btn-group">
+            <Button outline color="info">Все</Button>
             <button type="button" className="btn btn-info">Все посты</button>
             <button type="button" className="btn btn-outline-secondary">Понравились</button>
         </div>
